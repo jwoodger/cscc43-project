f0b6993e2b4b793554b358582445e926fe4f4c5a
jwoodger <woodger.jack@gmail.com>
2022-08-08 03:15:52 -0400

    Change table names to work on case-sensitive systems.

--- src/main/java/cscc43/mybnb/reports/GeneralReport.java
+++ src/main/java/cscc43/mybnb/reports/GeneralReport.java
@@ -15,48 +15,48 @@ public class GeneralReport {
         s.executeUpdate("drop view if exists location_details;");
         s.executeUpdate("create view location_details as \n" +
                 "select distinct Country, City,Postal_Code\n" +
-                "from mybnb.listing;");
+                "from mybnb.Listing;");
         s.executeUpdate("drop view if exists host_user;");
         s.executeUpdate("create view host_user as\n" +
-                "select username,host_id from user join host on User_ID = Host_ID;");
+                "select username,Host_ID from User join Host on User_ID = Host_ID;");
         s.executeUpdate("drop view if exists renter_user;");
         s.executeUpdate("create view renter_user as\n" +
-                "select username,renter_id from user join renter on User_ID = Renter_ID;");
+                "select username,Renter_ID from User join Renter on User_ID = Renter_ID;");
     }
     public void bookings()throws SQLException{
         int c = MenuUtils.menu("By city or city/zipcode","City","Zipcode");
         LocalDate low = MenuUtils.askDate("Start Date?");
         LocalDate up = MenuUtils.askDate("End Date?");
         if(c==1){
-            PreparedStatement s = connection.prepareStatement("select b.country,b.city,count(bookingid) as count\n" +
-                    "from (select * from booking natural join calendar_section natural join listing\n" +
-                    "\twhere date_from >= ? and date_to <= ?\n" +
-                    ") a right join listing b on a.listing_id = b.listing_id\n" +
-                    "group by country,city;");
+            PreparedStatement s = connection.prepareStatement("select b.Country,b.City,count(BookingId) as count\n" +
+                    "from (select * from Booking natural join Calendar_Section natural join Listing\n" +
+                    "\twhere Date_From >= ? and Date_To <= ?\n" +
+                    ") a right join Listing b on a.Listing_ID = b.Listing_ID\n" +
+                    "group by Country, City;");
             s.setDate(1,Date.valueOf(low));
             s.setDate(2,Date.valueOf(up));
             ResultSet r = s.executeQuery();
             System.out.println("Country     City     Count");
             while(r.next()){
 
-                System.out.println(r.getString("country")+"     "+r.getString("city")+
-                        "       "+r.getInt("count"));
+                System.out.println(r.getString("Country")+"     "+r.getString("City")+
+                        "       "+r.getInt("Count"));
 
             }
         }else{
-            PreparedStatement s = connection.prepareStatement("select b.country,b.city,b.postal_code,count(bookingid) as count\n" +
-                    "from (select * from booking natural join calendar_section natural join listing\n" +
-                    "\twhere date_from >= ? and date_to <= ?\n" +
-                    ") a right join listing b on a.listing_id = b.listing_id\n" +
-                    "group by country,city,postal_code;");
+            PreparedStatement s = connection.prepareStatement("select b.Country,b.City,b.Postal_Code,count(BookingId) as count\n" +
+                    "from (select * from Booking natural join Calendar_Section natural join Listing\n" +
+                    "\twhere Date_From >= ? and Date_To <= ?\n" +
+                    ") a right join Listing b on a.Listing_ID = b.Listing_ID\n" +
+                    "group by Country,City,Postal_Code;");
             s.setDate(1, Date.valueOf(low));
             s.setDate(2,Date.valueOf(up));
             ResultSet r = s.executeQuery();
             System.out.println("Country     City        ZipCode     Count");
             while(r.next()){
 
-                System.out.println(r.getString("country")+"     "+r.getString("city")+
-                        "       "+r.getString("postal_code")+"      "+r.getInt("count"));
+                System.out.println(r.getString("Country")+"     "+r.getString("City")+
+                        "       "+r.getString("Postal_Code")+"      "+r.getInt("Count"));
             }
         }
     }
@@ -66,27 +66,27 @@ public class GeneralReport {
         ResultSet r;
         switch (choice){
             case 1:
-                r = s.executeQuery("select country,count(*) as count\n" +
-                        "from listing\n" +
-                        "group by country;");
+                r = s.executeQuery("select Country,count(*) as count\n" +
+                        "from Listing\n" +
+                        "group by Country;");
                 System.out.println("Country`    Count");
                 while(r.next()){
                     System.out.println(r.getString("Country")+"     "+r.getString("Count"));
                 }
                 break;
             case 2:
-                r = s.executeQuery("select country,city,count(*) as count\n" +
-                        "from listing\n" +
-                        "group by country,city;");
+                r = s.executeQuery("select Country,City,count(*) as count\n" +
+                        "from Listing\n" +
+                        "group by Country,City;");
                 System.out.println("Country`    City    Count");
                 while(r.next()){
                     System.out.println(r.getString("Country")+"     "+r.getString("City")+"      "+r.getString("Count"));
                 }
                 break;
             case 3:
-                r = s.executeQuery("select country,city,Postal_Code,count(*) as count\n" +
+                r = s.executeQuery("select Country,City,Postal_Code,count(*) as count\n" +
                         "from listing\n" +
-                        "group by country,city,postal_code;");
+                        "group by Country,City,Postal_Code;");
                 System.out.println("Country     City    ZipCode     Count");
                 while(r.next()){
                     System.out.println(r.getString("Country")+"     "+r.getString("City")+"      "+r.getString("postal_code")+"     "+r.getString("Count"));
@@ -99,13 +99,13 @@ public class GeneralReport {
         Statement s = connection.createStatement();
         ResultSet r;
         if(choice ==1){
-            r = s.executeQuery("select a.country,username,count(Listing_ID) as count\n" +
+            r = s.executeQuery("select a.Country,username,count(Listing_ID) as count\n" +
                     "from\n" +
                     "(select Country,City,username,host_ID\n" +
                     "from  location_details l cross join host_user u\n" +
-                    "group by Country,City,username) a left join listing b on a.host_ID = b.host_ID and a.country=b.country and a.city=b.city\n" +
-                    "group by a.country,a.username\n" +
-                    "order by a.country,count(Listing_ID) desc;");
+                    "group by Country,City,username) a left join Listing b on a.Host_ID = b.Host_ID and a.Country=b.Country and a.City=b.City\n" +
+                    "group by a.Country,a.username\n" +
+                    "order by a.Country,count(Listing_ID) desc;");
             System.out.println("Country     Username    Count");
             while (r.next()){
                 System.out.println(r.getString("Country")+"     "+r.getString("Username")+"     "
@@ -113,13 +113,13 @@ public class GeneralReport {
             }
 
         }else{
-            r = s.executeQuery("select a.country,a.city,username,count(Listing_ID) as count\n" +
+            r = s.executeQuery("select a.Country,a.City,username,count(Listing_ID) as count\n" +
                     "from\n" +
-                    "(select Country,City,username,host_ID\n" +
+                    "(select Country,City,username,Host_ID\n" +
                     "from  location_details l cross join host_user u\n" +
-                    "group by Country,City,username) a left join listing b on a.host_ID = b.host_ID and a.country=b.country and a.city=b.city\n" +
-                    "group by a.country,a.city,a.username\n" +
-                    "order by a.country,a.city,count(Listing_ID) desc;");
+                    "group by Country,City,username) a left join Listing b on a.Host_ID = b.Host_ID and a.Country=b.Country and a.City=b.City\n" +
+                    "group by a.Country,a.City,a.username\n" +
+                    "order by a.Country,a.City,count(Listing_ID) desc;");
             System.out.println("Country     City    Username    Count");
             while (r.next()){
                 System.out.println(r.getString("Country")+"     "+r.getString("City")+"    "+
@@ -133,10 +133,10 @@ public class GeneralReport {
         ResultSet r = s.executeQuery("select Country,City,username\n" +
                 "from(\n" +
                 "select Country,City,username,count(*) as count\n" +
-                "from listing l natural join host_user\n" +
+                "from Listing l natural join host_user\n" +
                 "group by Country,City,username\n" +
                 ") L\n" +
-                "where count >0.1*(select count(*) from listing l where L.city = l.city and L.country = l.country);");
+                "where count >0.1*(select count(*) from Listing l where L.City = l.City and L.Country = l.Country);");
         System.out.println("Country     City    Username");
         while (r.next()){
             System.out.println(r.getString("Country")+"     "+r.getString("City")+"     "+
@@ -149,12 +149,12 @@ public class GeneralReport {
         LocalDate up = MenuUtils.askDate("Latest Date?");
 
         if(choice == 1){
-            PreparedStatement s = connection.prepareStatement("select country,city,username,count(bookingid) as count\n" +
-                    "from renter_user natural join booking natural join calendar_section natural join listing\n" +
+            PreparedStatement s = connection.prepareStatement("select Country,City,username,count(BookingId) as count\n" +
+                    "from renter_user natural join booking natural join Calendar_Section natural join Listing\n" +
                     "where ? <= date(BookedOn) and date(BookedOn) <= ?\n" +
-                    "group by country,city,username\n" +
-                    "having count(bookingid) >= 2\n" +
-                    "order by country,city,count(bookingid);");
+                    "group by Country,City,Username\n" +
+                    "having count(BookingId) >= 2\n" +
+                    "order by Country,City,count(BookingId);");
             s.setDate(1, Date.valueOf(low));
             s.setDate(2, Date.valueOf(up));
             ResultSet r = s.executeQuery();
@@ -167,7 +167,7 @@ public class GeneralReport {
 
             PreparedStatement s = connection.prepareStatement("\n" +
                     "select username,count(Bookingid) as count\n" +
-                    "from renter_user a left join (select * from booking where date(BookedOn)>= ? and date(BookedOn) <= ? ) b on a.renter_id = b.renter_id\n" +
+                    "from renter_user a left join (select * from Booking where date(BookedOn)>= ? and date(BookedOn) <= ? ) b on a.Renter_ID = b.Renter_ID\n" +
                     "group by username");
             s.setDate(1, Date.valueOf(low));
             s.setDate(2, Date.valueOf(up));
@@ -184,10 +184,10 @@ public class GeneralReport {
         LocalDate lowest = LocalDate.of(year,1,1);
         LocalDate highest = LocalDate.of(year,12,31);
         System.out.println("Showing cancelled bookings that were booked from "+lowest+" to "+highest);
-        PreparedStatement s = connection.prepareStatement("select b.username,count(bookingid) as count\n" +
-                "from (select * from renter_user natural join booking where cancelled = 1 and ?<=year(BookedOn) and year(BookedOn)<? ) a right join renter_user b on a.username = b.username\n" +
+        PreparedStatement s = connection.prepareStatement("select b.username,count(BookingId) as count\n" +
+                "from (select * from Renter_User natural join Booking where Cancelled = 1 and ?<=year(BookedOn) and year(BookedOn)<? ) a right join renter_user b on a.username = b.username\n" +
                 "group by b.username\n" +
-                "order by count(bookingid) desc\n" +
+                "order by count(BookingId) desc\n" +
                 "limit 5;");
         s.setInt(1,year);
         s.setInt(2,year+1);
@@ -197,12 +197,12 @@ public class GeneralReport {
         while (r.next()){
             System.out.println(r.getString("username")+"    "+r.getString("Count"));
         }
-        PreparedStatement s2 = connection.prepareStatement("select b.username, count(bookingid) as count\n" +
-                "from (select * from booking natural join calendar_section natural join listing natural join host natural join host_user where cancelled = 2 and\n" +
+        PreparedStatement s2 = connection.prepareStatement("select b.username, count(BookingId) as count\n" +
+                "from (select * from Booking natural join Calendar_Section natural join Listing natural join Host natural join host_user where cancelled = 2 and\n" +
                 " ?<=year(BookedOn) and year(BookedOn)<? ) a\n" +
                 "right join host_user b on a.username = b.username\n" +
                 "group by b.username\n" +
-                "order by count(bookingid) desc\n" +
+                "order by count(BookingId) desc\n" +
                 "limit 5;");
         s2.setInt(1,year);
         s2.setInt(2,year+1);
